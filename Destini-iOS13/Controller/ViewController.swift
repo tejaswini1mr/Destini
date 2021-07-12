1189eb81e1d509b7bdca676ab5c6ef018da5524a
Tejaswini M R <Tejaswini.MR@allegion.com>
2021-07-12 14:25:53 +0530

    add to master

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by Tejaswini MR on 08/08/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain=StoryBrain()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let titlex=sender.currentTitle!
+        storyBrain.nextStory(titlex)
+        updateUI()
+        
+        
+    }
+    
+    func updateUI(){
+        storyLabel.text=storyBrain.getTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+    }
+}
+
---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,19 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Tejaswini MR on 08/08/2019.
+//
+
+import Foundation
+
+struct Story {
+    let title:String
+    let choice1:String
+    let choice1Destination:Int
+    let choice2:String
+    let choice2Destination:Int
+    
+    
+    
+}
